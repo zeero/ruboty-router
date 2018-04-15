d0b413a1dbe79465b1d759fa7d0b5cba2e545841
zeero <zeero26@gmail.com>
2018-04-15 11:40:12 +0900

    refactor: reconsider test cases

--- test/ruboty/routers/sample_test.rb
+++ test/ruboty/routers/sample_test.rb
@@ -19,22 +19,18 @@ describe Ruboty::Routers::Sample do
       last_response.body.must_equal 'Sample!'
     end
 
-    it 'should chat' do
+    it 'should be able to chat' do
       Ruboty::Adapters::Shell.any_instance.expects(:say).with(body: 'something to say').once
       get '/sample'
     end
   end
 
   describe 'child' do
-    it 'should return 200' do
+    it 'should be able to define multiple route' do
+      Ruboty::Adapters::Shell.any_instance.expects(:say).with(body: 'something to say for child').once
       get '/sample/child'
       last_response.must_be :ok?
       last_response.body.must_equal 'Child!'
-    end
-
-    it 'should chat' do
-      Ruboty::Adapters::Shell.any_instance.expects(:say).with(body: 'something to say for child').once
-      get '/sample/child'
     end
   end
 end
